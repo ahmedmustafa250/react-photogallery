f8517fccc749d24c03d2614288182c8a0452d41c
Ahmed Mustafa <89400174+ahmedmustafa250@users.noreply.github.com>
2021-10-11 13:51:16 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import EditButton from "./components/editbutton/Editbutton";
 import Reaction from "./components/reaction/Reation";
 import Comment from "./components/comments/Comment";
 
+
 export default function App() {
   return (
     <div className="App-main">
+      <Photo/>
       <BadgeAvatars alt="Ahmed Mustafa" Date="June 18 2021" />
       <EditButton />
       <Reaction />
--- src/components/reaction/Reation.js
+++ src/components/reaction/Reation.js
@@ -2,8 +2,25 @@ import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import CommentIcon from "@mui/icons-material/Comment";
 import ScreenShareIcon from "@mui/icons-material/ScreenShare";
 import "./reaction.css";
+import likeIcon from "../../images/like.png"
+import thumbup from "../../images/thumb-up.png"
+import { useState } from "react";
+import Button from "../button/Button"
+import ShareButton from "../sharebutton/ShareButton"
+
 
 export default function Reaction() {
+  const [like, setLikestate] = useState(false)
+
+  const src=like ? thumbup : likeIcon
+
+  function change(){
+    console.log("Chal raha hoon")
+    setLikestate(!like);
+  }
+
+  
+
   return (
     <div>
       <div className="like-comment">
@@ -17,16 +34,19 @@ export default function Reaction() {
 
       <div className="reaction-top">
         <div>
-          <ThumbUpIcon />
-          <span className="like-comment-share">Like</span>{" "}
+          <img width="23px"  hight="50px" src={src}/>
+          <span className="like-comment-share" onClick={change}>Like</span>{" "}
         </div>
         <div>
           <CommentIcon />
           <span className="like-comment-share">Comments</span>
         </div>
-        <div>
+        <div style={{display:'flex'}}>
           <ScreenShareIcon />
-          <span className="like-comment-share">Share</span>
+          <span className="like-comment-share" >
+              <ShareButton text="share"  />
+          </span>
+          
         </div>
       </div>
     </div>
